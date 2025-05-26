b87df3419807447410cc1cbaabd1da2bf04fcae7
cauta <trongcauhcmus@gmail.com>
2025-05-26 19:45:54 +0700

    fix deployment

--- docker/copy.sh
+++ docker/copy.sh
@@ -9,7 +9,7 @@ usage() {
 
 # copy sql
 echo "begin copy sql "
-cp ../sql/ry_20240629.sql ./mysql/db
+cp ../sql/ry_20250523.sql ./mysql/db
 cp ../sql/ry_config_20250224.sql ./mysql/db
 
 # copy html
@@ -22,20 +22,26 @@ echo "begin copy ruoyi-gateway "
 cp ../ruoyi-gateway/target/ruoyi-gateway.jar ./ruoyi/gateway/jar
 
 echo "begin copy ruoyi-auth "
+rm ./ruoyi/auth/jar/ruoyi-auth.jar
 cp ../ruoyi-auth/target/ruoyi-auth.jar ./ruoyi/auth/jar
 
 echo "begin copy ruoyi-visual "
+rm ./ruoyi/visual/monitor/jar/ruoyi-visual-monitor.jar
 cp ../ruoyi-visual/ruoyi-monitor/target/ruoyi-visual-monitor.jar  ./ruoyi/visual/monitor/jar
 
 echo "begin copy ruoyi-modules-system "
+rm ./ruoyi/modules/system/jar/ruoyi-modules-system.jar
 cp ../ruoyi-modules/ruoyi-system/target/ruoyi-modules-system.jar ./ruoyi/modules/system/jar
 
 echo "begin copy ruoyi-modules-file "
+rm ./ruoyi/modules/file/jar/ruoyi-modules-file.jar
 cp ../ruoyi-modules/ruoyi-file/target/ruoyi-modules-file.jar ./ruoyi/modules/file/jar
 
 echo "begin copy ruoyi-modules-job "
+rm ./ruoyi/modules/job/jar/ruoyi-modules-job.jar
 cp ../ruoyi-modules/ruoyi-job/target/ruoyi-modules-job.jar ./ruoyi/modules/job/jar
 
 echo "begin copy ruoyi-modules-gen "
+rm ./ruoyi/modules/gen/jar/ruoyi-modules-gen.jar
 cp ../ruoyi-modules/ruoyi-gen/target/ruoyi-modules-gen.jar ./ruoyi/modules/gen/jar
 
